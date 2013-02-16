d8f9a2f7912d0098dcf4570b51a487c0c662f97e
steven <steven.johnson@drake.edu>
2013-02-16 11:52:27 -0600

    added second screen!

--- gen/edu/Drake/androidprojectone/R.java
+++ gen/edu/Drake/androidprojectone/R.java
@@ -15,23 +15,29 @@ public final class R {
         public static final int nettroll=0x7f020001;
     }
     public static final class id {
+        public static final int button1=0x7f070002;
         public static final int imageView1=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070003;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_second=0x7f030001;
+        public static final int activity_third=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
         public static final int activity_second=0x7f060001;
+        public static final int activity_third=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int coll=0x7f040006;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int net=0x7f040004;
         public static final int title_activity_second=0x7f040003;
+        public static final int title_activity_third=0x7f040005;
     }
     public static final class style {
         /** 
--- src/edu/Drake/androidprojectone/SecondActivity.java
+++ src/edu/Drake/androidprojectone/SecondActivity.java
@@ -1,15 +1,31 @@
 package edu.Drake.androidprojectone;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SecondActivity extends Activity {
+	
+	Button nextScreen;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_second);
+		
+		nextScreen = (Button) findViewById(R.id.button1);
+        nextScreen.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v)
+        	{
+        		Intent intent = new Intent(v.getContext(), ThirdActivity.class);
+        		startActivity(intent);
+        	}
+        });
 	}
 
 	@Override
